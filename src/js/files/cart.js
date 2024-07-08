2ac2ca39f1585d2830059b195e9b9b0c86e2acce
lina <lina@MacBook-Air-lina.local>
2024-07-08 10:55:16 +0300

    Add to cart function

--- src/js/files/cart.js
+++ src/js/files/cart.js
@@ -8,19 +8,42 @@ export function addToCart(productButton, productId) {
     const productImage = product.querySelector('.item-product__image');
 
     const productImageFly = productImage.cloneNode(true);
-    const productImageFlyWidth = productImage.offsetWidth
-    const productImageFlyHeight = productImage.offsetHeight
-    const productImageFlyTop = productImage.getBoundingClientRect().top
-    const productImageFlyLeft = productImage.getBoundingClientRect().left
-    productImageFly.setAttribute('class', '_flyimage ibg')
+    const productImageFlyWidth = productImage.offsetWidth;
+    const productImageFlyHeight = productImage.offsetHeight;
+    const productImageFlyTop = productImage.getBoundingClientRect().top;
+    const productImageFlyLeft = productImage.getBoundingClientRect().left;
+    productImageFly.setAttribute('class', '_flyimage ibg');
     productImageFly.style.cssText = `
-    left: ${productImageFlyLeft}px;
-    top: ${productImageFlyTop}px;
-    width: ${productImageFlyWidth}px;
-    height: ${productImageFlyHeight}px;
-    `;
-        console.log('productImageFly', productImageFly);
-        console.log('productImage', productImage);
+      left: ${productImageFlyLeft}px;
+      top: ${productImageFlyTop}px;
+      width: ${productImageFlyWidth}px;
+      height: ${productImageFlyHeight}px;`;
+    document.body.append(productImageFly);
 
+    const cartFlyTop = cart.getBoundingClientRect().top;
+    const cartImageFlyLeft = cart.getBoundingClientRect().left;
+
+    productImageFly.style.cssText = `
+      left: ${cartImageFlyLeft}px;
+      top: ${cartFlyTop}px;
+      height: 0px;
+      opacity: 0;
+      width: 0px;`;
+
+    productImageFly.addEventListener('transitionend', function () {
+      if (productButton.classList.contains('_fly')) {
+        productButton.classList.remove('_fly');
+        updateCart(productButton, productId);
+        productButton.remove();
+      }
+    });
   }
+}
+function  updateCart(productButton, productId, productAdd = true) {
+  const cart = document.querySelector('.cart-header')
+  const cartIcon = document.querySelector('.cart-header__icon')
+  const cartList = document.querySelector('.cart-header__icon')
+  const cartProduct = document.querySelector(`[data-cart-pi="${productId}"`);
+  const cartQuantity = cartIcon.querySelector('.cart-list')
+
 }
